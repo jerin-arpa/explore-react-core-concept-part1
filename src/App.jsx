0d1dd3de5e7b029dc6c79c1d6daa5ae7afe6eaf2
jerin-arpa <ismatjerrin3@gmail.com>
2023-09-07 04:03:43 +0600

    import export file

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todo from './todo'
 
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
-      <Device name="laptop" price="55000"></Device>
+      <h3>Vite + React</h3>
+      <Todo
+        task="Learn React"
+        isDone={true}></Todo>
+      <Todo
+        task="Core Concept"
+        isDone={false}></Todo>
+      <Todo
+        task="Try JSX"
+        isDone={true}></Todo>
+
+
+      {/* <Device name="laptop" price="55000"></Device>
       <Device name="mobile" price="17000"></Device>
       <Device name="watch" price="3000"></Device>
       <Person></Person>
       <Student grade="7" score="99"></Student>
+      <Student grade={12} score="85"></Student>
       <Student></Student>
-      <Student></Student>
-      <Developer></Developer>
+      <Developer></Developer> */}
     </>
   )
 }
@@ -33,13 +45,15 @@ function Person() {
 }
 
 
+
 const { grade, score } = { grade: '7', score: '99' };
-function Student(props) {
+
+function Student({ grade = 1, score = 0 }) {
   return (
     <div className='student'>
       <h3>This is a student</h3>
-      <p>Name:</p>
-      <p>Age:</p>
+      <p>Class: {grade}</p>
+      <p>Score: {score}</p>
     </div>
   )
 }
